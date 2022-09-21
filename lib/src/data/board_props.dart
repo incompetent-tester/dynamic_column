dbe2ef593cbe4753e9efc35f9c6ec395cc30f24e
incompetent-tester <incompetent.tester@gmail.com>
2022-09-21 11:38:29 +0800

    Added JSON serializable data classes

--- lib/src/data/board_props.dart
+++ lib/src/data/board_props.dart
@@ -1,8 +1,19 @@
+import 'dart:convert';
+
 class BoardXY {
   final int x;
   final int y;
 
   const BoardXY({this.x = 0, this.y = 0});
+
+  BoardXY.fromJson(Map<String, dynamic> json)
+      : x = json['x'],
+        y = json['y'];
+
+  Map<String, dynamic> toJson() => {
+        'x': x,
+        'y': y,
+      };
 }
 
 class BoardProps {
@@ -29,4 +40,17 @@ class BoardProps {
         ? other.id == id
         : other == id;
   }
+
+  BoardProps.fromJson(Map<String, dynamic> json)
+      : width = json['width'],
+        height = json['height'],
+        id = json['id'],
+        pos = BoardXY.fromJson(json['pos']);
+
+  Map<String, dynamic> toJson() => {
+        'width': width,
+        'height': height,
+        'id': id,
+        'pos': jsonEncode(pos),
+      };
 }
